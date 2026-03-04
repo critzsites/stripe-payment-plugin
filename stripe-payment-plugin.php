46ba64e9c58d064ee88433da81a0004c0f58b145
critzsites <caleb@critzsites.com>
2026-03-04 20:34:54 +0100

    Update stripe-payment-plugin.php
    Updating to version 1.0.3

--- stripe-payment-plugin.php
+++ stripe-payment-plugin.php
@@ -3,7 +3,7 @@
 * Plugin Name: Stripe Payment Plugin
 * Plugin URI: https://www.dynamicdreamz.com/
 * Description: A beautiful Stripe payment plugin with shortcode support and admin settings
-* Version: 1.0.2
+* Version: 1.0.3
 * Author: Dynamic Dreamz
 * Author URI: https://www.dynamicdreamz.com/
 * License: GPL v2 or later
@@ -16,7 +16,7 @@ exit;
 }
 
 // Define plugin constants
-define('STRIPE_PAYMENT_PLUGIN_VERSION', '1.0.2');
+define('STRIPE_PAYMENT_PLUGIN_VERSION', '1.0.3');
 define('STRIPE_PAYMENT_PLUGIN_DIR', plugin_dir_path(__FILE__));
 define('STRIPE_PAYMENT_PLUGIN_URL', plugin_dir_url(__FILE__));
 
